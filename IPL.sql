f547abcc8ba15e3043477ccd8b732a02d8f9d0e9
yatin1999 <91696223+yatin1999@users.noreply.github.com>
2022-03-14 11:47:41 +0530

    Update IPL.sql

--- IPL.sql
+++ IPL.sql
@@ -4,7 +4,7 @@ use IPL;
 
 /* Entity Tables */
 /* Table 1 */
-create table if not exists players (p_id varchar(4) primary key,
+create table if not exists players (p_id char(4) primary key,
                                     p_name varchar(50) not null,
                                     salary_in_cr float(20) not null,
                                     cap_id varchar(4),
@@ -17,7 +17,7 @@ insert into players values ('P103', 'V.S. Dhoni', 15, 'C103', 4);
 insert into players values ('P104', 'Dale Steyn', 8, 'C104', 2);
 
 /* Table 2 */
-create table if not exists players_performance (perf_id varchar(4) primary key,
+create table if not exists players_performance (perf_id char(4) primary key,
                                                 matches_no int,
                                                 runs int,
                                                 wickets int DEFAULT(0)
@@ -29,7 +29,7 @@ insert into players_performance(perf_id, matches_no, runs) values ('MS07', 220, 
 insert into players_performance values ('DS09', 95, 167, 97);
 
 /* Table 3 */
-create table if not exists teams (t_id varchar(4) primary key,
+create table if not exists teams (t_id char(4) primary key,
                                   t_name varchar(20) not null
                                   );
                                   
@@ -39,7 +39,7 @@ insert into teams values ('T103', 'Bangalore');
 insert into teams values ('T104', 'Punjab');
 
 /* Table 4 */
-create table if not exists game (matches_id varchar(3) primary key,
+create table if not exists game (matches_id char(3) primary key,
                                  win int DEFAULT(0),
                                  loss int DEFAULT(0)
                                  );
@@ -50,7 +50,7 @@ insert into game values ('M3', 11, 3);
 insert into game values ('M4', 9, 5);
 
 /* Table 5 */ 
-create table if not exists owner (owner_id varchar(4) primary key,
+create table if not exists owner (owner_id char(4) primary key,
                                   budget_in_cr int DEFAULT(90),
                                   spent_amt_in_cr float not null
                                   );
@@ -62,8 +62,8 @@ insert into owner(owner_id, spent_amt_in_cr) values ('O104', 89.55);
 
 /* Relationship Tables */
 /* Table 6 */
-create table if not exists rel_team_owner (t_id varchar(4) not null,
-                                           owner_id varchar(4) not null,
+create table if not exists rel_team_owner (t_id char(4) not null,
+                                           owner_id char(4) not null,
                                            t_name varchar(20) not null,
                                            budget_in_cr int DEFAULT(90),
                                            spent_amt_in_cr float not null,
@@ -78,7 +78,7 @@ insert into rel_team_owner(t_id, owner_id, t_name, spent_amt_in_cr) values ('T10
 insert into rel_team_owner(t_id, owner_id, t_name, spent_amt_in_cr) values ('T104', 'O104', 'Punjab', 89.55);
 
 /* Table 7 */
-create table if not exists rel_player_team (p_id varchar(4) not null,                                           
+create table if not exists rel_player_team (p_id char(4) not null,                                           
                                             p_name varchar(50) not null,
                                             salary_in_cr float(20) not null,
                                             cap_id varchar(4),
@@ -95,7 +95,7 @@ insert into rel_player_team values ('P103', 'M.S. Dhoni' , 15, 'C103', 4, 'T103'
 insert into rel_player_team values ('P104', 'Dale Steyn', 8, 'C104', 2, 'T104');
                                             
 /* Table 8 */
-create table if not exists rel_game_team (matches_id varchar(3) not null,
+create table if not exists rel_game_team (matches_id char(3) not null,
                                           t_id varchar(4) not null,
                                           primary key (matches_id, t_id),
                                           foreign key (matches_id) references game(matches_id),
@@ -108,8 +108,8 @@ insert into rel_game_team values ('M3', 'T103');
 insert into rel_game_team values ('M4', 'T104');
                           
 /* Table 9 */                                
-create table if not exists rel_player_player_performance (p_id varchar(4) not null,
-                                                          perf_id varchar(4) not null,
+create table if not exists rel_player_player_performance (p_id char(4) not null,
+                                                          perf_id char(4) not null,
                                                           primary key (p_id, perf_id),
                                                           foreign key (p_id) references players(p_id),
                                                           foreign key (perf_id) references players_performance(perf_id)
